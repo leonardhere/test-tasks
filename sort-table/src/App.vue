caac6fa9fc1e69723600135787906185ae42e6af
Percival Leonard <anrelized.kun@gmail.com>
2021-07-09 11:04:24 +0300

    first commit

---
+++ sort-table/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1>Таблица VUE</h1>
+    <table>
+      <tr><th>Пользователь</th><th><a href="#">Возраст</a></th><th>Дата рождения</th></tr>
+      <tr v-for="user in users" :key="user.id"><td>{{user.name}}</td><td>{{user.age}}</td><td>{{user.datebirth}}</td></tr>
+    </table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data() {
+    return {
+      users: [ 
+        { id: 1, name: 'Андрей', age: 18, datebirth: '10.05.2003' } , 
+        { id: 2, name: 'Игорь', age: 20, datebirth: '17.08.2001' }, 
+        { id: 3, name: 'Ольга', age: 23, datebirth: '03.04.1998' }, 
+        { id: 4, name: 'Никита', age: 19, datebirth: '08.06.2002' },
+        { id: 5, name: 'Оксана', age: 30, datebirth: '03.09.1991' }
+         ]
+      }
+  },
+    methods: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+    table {
+      border-collapse: collapse;
+    }
+    th, td {
+      border: 1px solid black;
+    }
+</style>
